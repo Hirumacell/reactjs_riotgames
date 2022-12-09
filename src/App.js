1c57fdd3b178f5e8554d13a4023f708f0bb5fd03
VictorTerrien <victor.terrien@epsi.fr>
2022-12-09 16:37:30 +0100

    tiens

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import Button from 'react-bootstrap/Button';
 function App() {
     const [searchText, setSearchText] = useState("");
     const [playerDataLol, setPlayerDataLol] = useState({});
-    const [playerTop3, setPlayertop3] = useState({})
-    const API_KEY = "RGAPI-7ed697fd-ea1a-4600-95d2-c9b4a09f160c";
+    const API_KEY = "RGAPI-3931cc47-b39e-4e48-8963-2b50feae4609";
 
     function searchForPlayerbyName(event) {
         // Serveur de l'API
@@ -22,7 +21,17 @@ function App() {
         });
     }
 
+    const [playerTop3, setPlayertop3] = useState({});
 
+    function search3TopChampions(event) {
+     var APICallString = "https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + playerDataLol.id + "/top?count=3&apikey=" + API_KEY;
+
+     axios.get(APICallString).then(function (response) {
+         setPlayertop3(response.data);
+     }).catch(function (error) {
+         console.log(error);
+     });
+    }
 
     return (
         <div className="App">
@@ -36,7 +45,6 @@ function App() {
                 {JSON.stringify(playerDataLol) != '{}' ?
                     <>
                         <p> {playerDataLol.name} </p>
-                        <p> {playerDataLol.id} </p>
                         <p> Niveau : {playerDataLol.summonerLevel} </p>
                         <img width="100" height="100"
                              src={"http://ddragon.leagueoflegends.com/cdn/12.23.1/img/profileicon/" + playerDataLol.profileIconId + ".png"}/>
@@ -46,8 +54,18 @@ function App() {
                 }
             </div>
             <div>
-                <button>Voir les champions les plus joués</button>
-
+                <input type="hidden" onChange={e => setPlayertop3(e.target.value)}/>
+                <button onClick={e => search3TopChampions(e)}>Voir les champions les plus joués</button>
+            </div>
+            <div>
+                {JSON.stringify(playerTop3) != '{}' ?
+                    <>
+                        <p>{playerDataLol.id}</p>
+                        <p> {playerTop3[0].championId} </p>
+                    </>
+                    :
+                    <> <p>No info yet</p> </>
+                }
             </div>
         </div>
     );
