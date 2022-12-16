03cc042186defbf38276427c94501e425d073874
VictorTerrien <victor.terrien@epsi.fr>
2022-12-16 15:08:19 +0100

    Fin

--- src/App.js
+++ src/App.js
@@ -1,80 +1,62 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import './App.css';
 
 function App() {
     const [searchText, setSearchText] = useState("");
-    const [playerDataLol, setPlayerDataLol] = useState({});
-    const [playerTop3, setPlayertop3] = useState({});
-    const [Champ, setChamp] = useState({});
-
-    const [champion1, setChampion1] = useState("");
-    const [champion2, setChampion2] = useState("");
-    const [champion3, setChampion3] = useState("");
+    const [champions, setChampions] = useState([]);
+    const [summonerName, setSummonerName] = useState();
+    const [playerDataLol, setPlayerDataLol] = useState();
+    const [playerTop3, setPlayertop3] = useState([]);
 
     const API_KEY = "RGAPI-e932c593-6ac5-44f8-a6b4-a5cac969de69";
 
-    function searchForPlayerbyName(event) {
-        // Serveur de l'API
-        var APICallString = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + searchText + "?api_key=" + API_KEY;
-        // Utilisation de AXIOS
-        axios.get(APICallString).then(function (response) {
-            setPlayerDataLol(response.data);
+    useEffect(() => {
+        const ChampsURL = "https://raw.githubusercontent.com/ngryman/lol-champions/master/champions.json";
 
+        axios.get(ChampsURL).then(function (response) {
+            setChampions(response.data);
         }).catch(function (error) {
-            console.log(error)
+            console.log(error);
         });
+    }, []);
 
-
-        function Search3() {
-            var APICallChamp = "https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + playerDataLol.id + "/top?count=3&api_key=" + API_KEY;
-
-            axios.get(APICallChamp).then(function (response1) {
-                setPlayertop3(response1.data);
-            }).catch(function (error1) {
-                console.log(error1);
+    useEffect(() => {
+        if (summonerName) {
+            const APICallString = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + searchText + "?api_key=" + API_KEY;
+            axios.get(APICallString).then(function (response) {
+                setPlayerDataLol(response.data);
+            }).catch(function (error) {
+                console.log(error)
             });
         }
+    }, [summonerName]);
 
-        function SearchChamps() {
-            var ChampsURL = "https://raw.githubusercontent.com/ngryman/lol-champions/master/champions.json";
+    useEffect(() => {
+        if (playerDataLol) {
+            const APICallChamp = "https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + playerDataLol.id + "/top?count=3&api_key=" + API_KEY;
 
-            axios.get(ChampsURL).then(function (response2) {
-                setChamp(response2.data);
-            }).catch(function (error2) {
-                console.log(error2);
+            axios.get(APICallChamp).then(function (response) {
+                setPlayertop3(response.data);
+            }).catch(function (error) {
+                console.log(error);
             });
         }
+    }, [playerDataLol]);
 
-        Search3();
-        SearchChamps();
-
-        console.log(Champ);
-        console.log(playerTop3);
-
-        for (let i = 0; i < 153; i++) {
-            if (parseInt(Champ[i].key) == parseInt(playerTop3[0].championId)) {
-                setChampion1(Champ[i].id);
-            }
-            if (parseInt(Champ[i].key) == parseInt(playerTop3[1].championId)) {
-                setChampion2(Champ[i].id);
-            }
-            if (parseInt(Champ[i].key) == parseInt(playerTop3[2].championId)) {
-                setChampion3(Champ[i].id);
-            }
-        }
+    function handleSubmit() {
+        setSummonerName(searchText);
     }
 
     return (
         <div className="App">
-
             <div className="container">
-                <h5>LeagueOfLegends Player Searcher</h5>
+                <h2>LeagueOfLegends recherche des joueurs</h2>
                 <input type="text" onChange={e => setSearchText(e.target.value)}></input>
-                <button onClick={e => searchForPlayerbyName(e)}>Search for player</button>
+                <button onClick={handleSubmit}>OK</button>
             </div>
             <div>
-                {JSON.stringify(playerDataLol) != '{}' ?
+                {!!playerDataLol ?
                     <>
                         <p> {playerDataLol.name} </p>
                         <p> Niveau : {playerDataLol.summonerLevel} </p>
@@ -84,21 +66,16 @@ function App() {
                     :
                     <> <p> Y'a pas de joueur avec ce nom </p> </>
                 }
-                {JSON.stringify(playerTop3) != '{}' ?
-                    <>
-                        <p>Top 1 : {champion1.charAt(0).toUpperCase() + champion1.slice(1)} avec {playerTop3[0].championPoints} points</p>
-                        <img width="100" height="100"
-                             src={"http://ddragon.leagueoflegends.com/cdn/12.23.1/img/champion/" + champion1.charAt(0).toUpperCase() + champion1.slice(1) + ".png"}/>
-                        <p>Top 2 : {champion2.charAt(0).toUpperCase() + champion2.slice(1)} avec {playerTop3[1].championPoints} points</p>
-                        <img width="100" height="100"
-                             src={"http://ddragon.leagueoflegends.com/cdn/12.23.1/img/champion/" + champion2.charAt(0).toUpperCase() + champion2.slice(1) + ".png"}/>
-                        <p>Top 3 : {champion3.charAt(0).toUpperCase() + champion3.slice(1)} avec {playerTop3[2].championPoints} points</p>
-                        <img width="100" height="100"
-                             src={"http://ddragon.leagueoflegends.com/cdn/12.23.1/img/champion/" + champion3.charAt(0).toUpperCase() + champion3.slice(1) + ".png"}/>
-                    </>
-                    :
-                    <> <p>Pas encore de joueur sélectionné</p> </>
-                }
+                <h2>Top 3 champions</h2>
+                <ul>
+                    {playerTop3.map((playerTop) => {
+                        const champion = champions.find(champ => parseInt(champ.key) === playerTop.championId);
+                        return <li key={playerTop.championId}>
+                            <p>{champion.name} avec {playerTop.championPoints} points</p>
+                            <img width="100" height="100" src={champion.icon}/>
+                        </li>}
+                    )}
+                </ul>
             </div>
         </div>
     );
